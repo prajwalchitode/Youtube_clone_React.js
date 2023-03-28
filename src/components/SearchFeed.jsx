713f0a9ea057f1bedf2d6b776c2102ca90a1c2e8
Prajwal Chitode <99211609+prajwalchitode@users.noreply.github.com>
2023-03-29 00:54:41 +0530

    final

---
+++ src/components/SearchFeed.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { useState , useEffect } from 'react'
+import {Box ,  Typography} from "@mui/material"
+// import Categories from './Sidebar'
+import Videos from "./Video"
+import { fetchFromApi } from '../utils/fetchFromApi'
+import { useParams } from 'react-router-dom'
+
+
+const SearchFeed = () => {
+
+  const [videos , setVideos] = useState([]);
+  const {searchTerm} = useParams();
+
+
+  useEffect(()=>{
+    fetchFromApi(`search?part=snippet&q=${searchTerm}`).then((data)=>
+    setVideos(data.items))
+  },[searchTerm]);
+
+
+  return (
+
+
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+          Search Result For: <span style={{ color: "#FC1503" }}>{searchTerm}</span> Videos
+        </Typography>
+
+        <Videos videos={videos}/>
+        
+      </Box>
+  
+   
+  )
+}
+
+export default SearchFeed;
